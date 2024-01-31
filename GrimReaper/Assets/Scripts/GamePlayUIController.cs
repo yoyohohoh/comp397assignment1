5e897c5e171e987406920d97aebb394a5f71b422
Yiu Yiu Yoyo Ho <yho45@my.centennialcollege.ca>
2024-01-31 18:10:41 -0500

    Part 2: Save Msg, Health Bar

--- GrimReaper/Assets/Scripts/GamePlayUIController.cs
+++ GrimReaper/Assets/Scripts/GamePlayUIController.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GamePlayUIController : MonoBehaviour
 {
     [SerializeField] GameObject pauseMsg;
+    [SerializeField] GameObject saveMsg;
+    [SerializeField] Button saveBtn;
+
 
     public void Start()
     {
         pauseMsg.SetActive(false);
+        saveMsg.SetActive(false);
     }
 
     public void PauseGame()
@@ -20,4 +25,20 @@ public class GamePlayUIController : MonoBehaviour
     {
         pauseMsg.SetActive(false);
     }
+
+    public void SaveGame()
+    {
+        saveMsg.SetActive(true);
+        //saveBtn button color change to normal
+        saveBtn.interactable = false;
+        saveBtn.interactable = true;
+        Invoke("CloseSaveMsg", 1);
+    }
+
+    public void CloseSaveMsg()
+    {
+        saveMsg.SetActive(false);
+    }
+
+
 }
