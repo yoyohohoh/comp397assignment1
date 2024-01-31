f868d6ecc0935788faa8c63055a7f587b3e21fec
Yiu Yiu Yoyo Ho <yho45@my.centennialcollege.ca>
2024-01-30 21:13:33 -0500

    Part 2: add DataKeeper (Basic)

--- GrimReaper/Assets/Scripts/DataKeeper.cs
+++ GrimReaper/Assets/Scripts/DataKeeper.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DataKeeper : MonoBehaviour
 {
     public static DataKeeper Instance;
+
+    public float musicVolume;
+    public float soundVolume;
+
     private void Awake()
     {
         if (Instance != null)
@@ -15,5 +20,11 @@ public class DataKeeper : MonoBehaviour
 
         Instance = this;
         DontDestroyOnLoad(gameObject);
+    }
+
+    public void Update()
+    {
+        DataKeeper.Instance.musicVolume = musicOption.GetComponent<Slider>().value;
+        DataKeeper.Instance.soundVolume = soundOption.GetComponent<Slider>().value;
     }
 }
