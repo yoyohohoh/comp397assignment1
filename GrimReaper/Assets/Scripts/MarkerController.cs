bc95f6c1023018f910bc30acf215d0a0fc8ad1df
yoyohohoh <yho45@my.centennialcollege.ca>
2024-02-08 13:42:49 -0500

    added enemy markers

---
+++ GrimReaper/Assets/Scripts/MarkerController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarkerController : MonoBehaviour
+{
+    [SerializeField] private GameObject target;
+
+    void Update()
+    {
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, target.transform.position.z);
+        transform.rotation = Quaternion.Euler(90, 0, 0);
+    }
+}
