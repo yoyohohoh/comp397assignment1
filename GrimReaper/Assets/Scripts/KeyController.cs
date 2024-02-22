9868ccc17faaf411524d84b4cf7fbde5f96f32a4
Yiu Yiu Yoyo Ho <yho45@my.centennialcollege.ca>
2024-02-21 23:12:55 -0500

    Part 3: added Key for going to next level

--- GrimReaper/Assets/Scripts/KeyController.cs
+++ GrimReaper/Assets/Scripts/KeyController.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class KeyController : MonoBehaviour
 {
+    public static KeyController _instance;
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public bool hasKey = false;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             SoundController.instance.Play("Item");
+            hasKey = true;
             Destroy(gameObject);
         }
     }
